6c48356b5767341e4d42b3a38ccff4429fd0641d
Mohamad Abdelhaq <mohamadabd4112002@gmail.com>
2023-08-24 17:36:29 +0300

    fix

--- src/posts/AddNewPost.tsx
+++ src/posts/AddNewPost.tsx
@@ -1,30 +1,34 @@
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 import "./AddNewPost.scss"
 import { Formik, Form, Field } from 'formik'
 import { Dropdown } from 'primereact/dropdown';
 import * as Yup from 'yup'
 import { dataAPI } from './data-api'
 import { useNavigate } from 'react-router-dom'
-import Spinner from './Spinner';
 import { useAppContext } from '../helpers/app-store';
+import 'primereact/resources/themes/saga-blue/theme.css';
+import 'primereact/resources/primereact.min.css';
+import 'primeicons/primeicons.css';
+
 
   const countries = [
-    { label: 'Palestine', value: 'Palestine' },
-    { label: 'Tunisia', value: 'Tunisia' },
+    { label: "Palestine", value: "Palestine" },
+    { label: "Tunisia", value: "Tunisia" },
+    { label: "Indonesia", value: "Indonesia" },
+    { label: "China", value: "China" },
+    { label: "France", value: "France" },
+    { label: "Russia", value: "Russia" },
+    { label: "Portugal", value: "Portugal" },
+    { label: "Venezuela", value: "Venezuela" },
+    { label: "Poland", value: "Poland" },
+
   ];
   
 const AddNewPost = () => {
     const { username } =useAppContext();
     const navigate = useNavigate();
-    const [nextId, setNextId] = useState(101);
-    const [country, setCountry ]= useState('');
-    useEffect(() => {
-      const storedNextId = localStorage.getItem('nextId');
-      if (storedNextId) {
-        setNextId(parseInt(storedNextId));
-      }
-    }, []);
-  
+    const [country, setCountry ]= useState();
+
     const initialValues = {
         title: '',
         country: '',
@@ -36,9 +40,8 @@ const AddNewPost = () => {
         title: Yup.string().required('Title is required').min(5, 'Title must be at least 5 characters'),
       });    
       const onSubmit = async (values: typeof initialValues, { setSubmitting }: { setSubmitting: (isSubmitting: boolean) => void }) => {
-        setNextId(nextId => nextId + 1);
-        localStorage.setItem('nextId', String(nextId + 1));
         const now = new Date().toISOString();
+        const randomId = Math.floor(Math.random() * 900) + 101;
         const newPost = {
             hashtags: values.title,
             country: values.country,
@@ -49,11 +52,10 @@ const AddNewPost = () => {
             shares: 0,
             user_name: username,
             user_avatar: "https://api.dicebear.com/6.x/avataaars/svg?backgroundColor=c0aede&seed=d",
-            user_id: nextId,
+            user_id: randomId,
             image_url: values.img,
             timestamp: now,
           };
-          <Spinner/>
           setSubmitting(true);
           try {
             await dataAPI.addPost(newPost);
@@ -80,8 +82,16 @@ const AddNewPost = () => {
             </div>
             <div className="form-group">
               <label htmlFor="country">Country</label>
-              <Dropdown id="country" name="country" options={countries} optionLabel="label" optionValue="value"
-              placeholder='choose your country' onChange={(e)=> setCountry(e.value)}/>
+              <Dropdown
+                inputId="country"
+                name="country"
+                value={country}
+                options={countries}
+                optionLabel="label"
+                optionValue="value"
+                placeholder="Choose your country"
+                onChange={(e) => setCountry(e.value)}
+              />
             </div>
             <div className="form-group-check">
               <label htmlFor='ver'>Verified</label>
